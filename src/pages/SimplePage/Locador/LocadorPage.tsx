d47cfcd5afa741354a7de6c5b7ceb8ffbfe851cb
brugondim <90776432+brugondim@users.noreply.github.com>
2024-05-17 17:47:29 -0300

    Update LocadorPage.tsx

--- src/pages/SimplePage/Locador/LocadorPage.tsx
+++ src/pages/SimplePage/Locador/LocadorPage.tsx
@@ -65,7 +65,7 @@ export function LocadorPage() {
           </div>
           <div className="flex items-center">
           <Link
-            to={'/infosQuadra/' + props.idEmpresa}
+            to={'/empresa/'}
             className="px-5 py-2 text-green-600 border rounded-sm border-green-500 hover:bg-green-500 hover:text-white"
           >
             Minhas Quadras
