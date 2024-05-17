d5452b588f2635127c4ce174b104d573b81c7d05
brugondim <90776432+brugondim@users.noreply.github.com>
2024-05-17 17:31:40 -0300

    Update Card.tsx

--- src/pages/SimplePage/Search/Card/Card.tsx
+++ src/pages/SimplePage/Search/Card/Card.tsx
@@ -61,7 +61,7 @@ export function Card({ props }: CardProps) {
           </Link>
 
           <Link
-            to={'/empresa/' + props.idEmpresa}
+            to={'/infosQuadra/' + props.idEmpresa}
             className="px-5 py-2 text-green-600 border rounded-sm border-green-500 hover:bg-green-500 hover:text-white"
           >
             VER TODAS AS QUADRAS
