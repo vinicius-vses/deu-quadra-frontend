69ae5cf791faed8901ab80c9ccec1b1367437a24
brugondim <90776432+brugondim@users.noreply.github.com>
2024-05-22 15:23:57 -0300

    Login

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,9 +3,12 @@ import { useAuth } from '../hooks/Auth';
 import { GetCourtsOutput, LoginOutput, GetEmpresasOutput } from './endpointsOutput';
 
 export interface AuthResult {
-  login: (email: string, password: string)=> Promise<AxiosResponse<LoginOutput, any>>;
-  getCourts: (lat:number,lon: number, distanceInKillometers: number)=> Promise<AxiosResponse<GetCourtsOutput[], any>>;
-  getEmpresas: (lat:number,lon: number, km: number)=> Promise<AxiosResponse<GetEmpresasOutput[], any>>;
+  loginLocador: (email: string, password: string) => Promise<AxiosResponse<LoginOutput, any>>;
+  loginLocatario: (email: string, password: string) => Promise<AxiosResponse<LoginOutput, any>>;
+  getCourts: (lat: number, lon: number, distanceInKillometers: number) => Promise<AxiosResponse<GetCourtsOutput[], any>>;
+  getEmpresas: (lat: number, lon: number, km: number) => Promise<AxiosResponse<GetEmpresasOutput[], any>>;
+  getEmpresasLatLong: (lat: number, lon: number) => Promise<AxiosResponse<GetEmpresasOutput[], any>>;
+  getQuadrasLatLong: (lat: number, lon: number) => Promise<AxiosResponse<GetCourtsOutput[], any>>;
 }
 
 export function useApi(): AuthResult {
@@ -16,51 +19,44 @@ export function useApi(): AuthResult {
     baseURL: import.meta.env.VITE_API_URL,
   });
 
-  async function login(email: string, password: string): Promise<AxiosResponse<LoginOutput, any>> {
+  async function loginLocador(email: string, password: string): Promise<AxiosResponse<LoginOutput, any>> {
     return client.post<LoginOutput>('/locador/login', {
       email,
       password,
     });
   }
 
-  async function getEmpresas(lat:number,lon: number, km: number): Promise<AxiosResponse<GetEmpresasOutput[], any>> {
-    return client.post<GetEmpresasOutput[]>('/companies/search',{
-        lat,lon,km
+  async function loginLocatario(email: string, password: string): Promise<AxiosResponse<LoginOutput, any>> {
+    return client.post<LoginOutput>('/locatario/login', {
+      email,
+      password,
     });
   }
 
-  async function getCourts(lat:number,lon: number, distanceInKillometers: number): Promise<AxiosResponse<GetCourtsOutput[], any>> {
-    return client.post<GetCourtsOutput[]>('/courts/search',{
-        coordinates: {
-          lat,lon
-      },
-      properties: {
-          distanceInKillometers
-      }
-    })
+  async function getEmpresas(lat: number, lon: number, km: number): Promise<AxiosResponse<GetEmpresasOutput[], any>> {
+    return client.post<GetEmpresasOutput[]>('/companies/search', {
+      lat, lon, km
+    });
   }
 
-  async function getEmpresasLatLong(lat:number,lon: number): Promise<AxiosResponse<GetEmpresasOutput[], any>> {
-    return client.get<GetEmpresasOutput[]>('/companies',{
-        coordinates: {
-          lat,lon
-      },
-      properties: {
-        lat,lon
-    }
-    })
+  async function getCourts(lat: number, lon: number, distanceInKillometers: number): Promise<AxiosResponse<GetCourtsOutput[], any>> {
+    return client.post<GetCourtsOutput[]>('/courts/search', {
+      coordinates: { lat, lon },
+      properties: { distanceInKillometers }
+    });
   }
 
-  async function getQuadrasLatLong(lat:number,lon: number): Promise<AxiosResponse<GetCourtsOutput[], any>> {
-    return client.get<GetCourtsOutput[]>('/courts',{
-        coordinates: {
-          lat,lon
-      },
-      properties: {
-        lat,lon
-    }
-    })
+  async function getEmpresasLatLong(lat: number, lon: number): Promise<AxiosResponse<GetEmpresasOutput[], any>> {
+    return client.get<GetEmpresasOutput[]>('/companies', {
+      params: { lat, lon }
+    });
   }
 
-  return { login, getCourts, getEmpresas, getEmpresasLatLong };
+  async function getQuadrasLatLong(lat: number, lon: number): Promise<AxiosResponse<GetCourtsOutput[], any>> {
+    return client.get<GetCourtsOutput[]>('/courts', {
+      params: { lat, lon }
+    });
+  }
+
+  return { loginLocador, loginLocatario, getCourts, getEmpresas, getEmpresasLatLong, getQuadrasLatLong };
 }
--- src/pages/SimplePage/Login/Actions/Login.tsx
+++ src/pages/SimplePage/Login/Actions/Login.tsx
@@ -9,6 +9,7 @@ import { LanguageContext } from '../../../../contexts/Language';
 export function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [userType, setUserType] = useState('locador'); // Default to 'locador'
 
   const auth = useContext(AuthenticationContext);
   const navigate = useNavigate();
@@ -17,33 +18,38 @@ export function Login() {
     if (auth?.isAuthenticated) {
       navigate('/');
     }
-  }, []);
-  console.log(auth);
+  }, [auth, navigate]);
 
-  const { login } = useApi();
+  const { loginLocador, loginLocatario } = useApi();
   const { openModal } = useModal();
   const { language } = useContext(LanguageContext)!;
 
   function handleLogin(event: FormEvent) {
     event.preventDefault();
 
-    login(email, password)
-      .then(({ data }) => {
-        auth!.setTokens({
-          authToken: {
-            token: data,
-            expiresIn: new Date(),
-          },
-          refreshToken: {
-            expiresIn: new Date(),
-            token: 'a',
-          },
-        });
+    const loginFunction = userType === 'locador' ? loginLocador : loginLocatario;
 
-        navigate('/empresa');
+    loginFunction(email, password)
+      .then((response) => {
+        if (response.data) {
+          auth!.setTokens({
+            authToken: {
+              token: response.data,
+              expiresIn: new Date(),
+            },
+            refreshToken: {
+              expiresIn: new Date(),
+              token: 'a',
+            },
+          });
+
+          navigate('/empresa');
+        } else {
+          openModal('Erro', 'Erro desconhecido ao fazer login');
+        }
       })
       .catch((error: any) => {
-        openModal('Erro', error.response.data.message);
+        openModal('Erro', error.response ? error.response.data.message : 'Erro desconhecido ao fazer login');
       });
   }
 
@@ -51,6 +57,21 @@ export function Login() {
     <div className="p-6 bg-white shadow-md rounded-lg mb-3">
       <h2 className="text-2xl font-semibold mb-4 text-center">Login</h2>
       <form onSubmit={handleLogin}>
+        <div className="mb-4">
+          <label htmlFor="userType" className="block text-sm font-medium text-gray-700">
+            Tipo de Usuário
+          </label>
+          <select
+            id="userType"
+            name="userType"
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            value={userType}
+            onChange={(e) => setUserType(e.target.value)}
+          >
+            <option value="locador">Locador</option>
+            <option value="locatario">Locatário</option>
+          </select>
+        </div>
         <TextInput
           label="Email"
           type="email"
@@ -67,12 +88,13 @@ export function Login() {
           onChange={(e) => setPassword(e.target.value)}
           placeholder={language.passwordButtonPlaceholder}
         />
-         <div className="flex justify-between items-center mt-3">
-          <a href="/Reset" className="text-sm text-blue-500 hover:underline">Esqueci minha senha</a>
-          </div>
-               <div className="flex w-full justify-between px-2 mt-3">
-          
-        <button
+        <div className="flex justify-between items-center mt-3">
+          <a href="/Reset" className="text-sm text-blue-500 hover:underline">
+            Esqueci minha senha
+          </a>
+        </div>
+        <div className="flex w-full justify-between px-2 mt-3">
+          <button
             type="button"
             className="bg-green-500 text-white p-2 rounded-md mt-3 mx-auto hover:bg-blue-600"
             onClick={() => navigate('/')}
