3cd9363f1af67a0cb4ec95d739293b5641017d9c
brugondim <90776432+brugondim@users.noreply.github.com>
2024-05-22 13:04:21 -0300

    Update infoquadras

--- src/pages/SimplePage/Locador/Card/CardEmpresa.tsx
+++ src/pages/SimplePage/Locador/Card/CardEmpresa.tsx
@@ -72,7 +72,7 @@ export function CardEmpresa({ props }: CardProps) {
           </Link>
 
           <Link
-            to={'/empresa/' + 1}
+            to={'/infosQuadra/' + props.idEmpresa}
             className="px-5 py-2 text-green-600 border rounded-sm border-green-500 hover:bg-green-500 hover:text-white"
           >
             VER TODAS AS QUADRAS
