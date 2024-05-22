93d9d191702351154527bfb6cc41e54d6a33ada5
Estefanie Coleone <84871976+estefaniecoleone@users.noreply.github.com>
2024-05-22 00:25:32 -0300

    user id

--- src/pages/SimplePage/Locador/LocadorPage.tsx
+++ src/pages/SimplePage/Locador/LocadorPage.tsx
@@ -4,16 +4,23 @@ import Navbar from '../../../components/Navbar/Navbar';
 import { CardLocador } from '../Locador/Card/CardLocador';
 import { CardEmpresa } from '../Locador/Card/CardEmpresa';
 import { Link, useParams } from 'react-router-dom';
+import { useAuth } from '../../../hooks/Auth';
+
 
 import axios from 'axios';
+import { CardQuadra } from './Card/CardQuadra';
 
 export function LocadorPage() {
 
   const [courtsData, setCourtsData] = useState([]);
+  const [companiesData, setCompaniesData] = useState([]);
+
   const [testeData, setTeste] = useState([]);
 
-
-  const idEmpresa = 4;
+  const authentication = useAuth();
+  const idUsuario = authentication.user?.id;
+  console.log(idUsuario);
+  const idEmpresa = 1;
 
   let nomeEmpresa = [];
 
@@ -21,16 +28,19 @@ export function LocadorPage() {
 
   useEffect(() => {
     // Faz a requisição para o endpoint
-    axios.get('http://18.212.67.172:8080/companies/' + idEmpresa)
+    axios.get('http://18.212.67.172:8080/companies/empresa/' + idUsuario)
+    //axios.get('http://localhost:8080/companies/empresa/' + idUsuario)
         .then((response) => {
         // Atualiza o estado com os dados recebidos
         //setCourtsData(response.data.courtsModelList);
         nomeEmpresa = response.data.nome;
-        setEmpresa(response.data.nome);
-        setEmpresa(response.data.rua);
-
-        setCourtsData(response.data.courtsModelList);
-        console.log("oie");
+        // setEmpresa(response.data.nome);
+        // setEmpresa(response.data.bairro);
+        // setEmpresa(response.data.rua);
+        // setEmpresa(response.data.numero);
+        setEmpresa(response.data);
+        console.log(response.data);
+        console.log(empresaData);
         })
         .catch((error) => {
         console.error('Erro ao buscar dados das quadras:', error);
@@ -43,9 +53,9 @@ export function LocadorPage() {
 
           <Navbar className="fade-in"></Navbar>
           <div className="mt-8 flex flex-col items-center border bg-white space-y-4">
-            <h2>Quadras da empresa: </h2>
-            {courtsData.map((data, idEmpresa) => {
-              return <CardLocador key={idEmpresa} props={data}></CardLocador>;
+            <h2>Empresas cadastradas: </h2>
+            {empresaData?.map((data, idEmpresa) => {
+              return <CardEmpresa key={idEmpresa} props={data}></CardEmpresa>;
             })}
             </div>
     
@@ -80,12 +90,11 @@ export function LocadorPage() {
           </Link>
           </div>
           <Link
-            to={'/novaQuadra/'}
+            to={'/novaQuadra/'} 
             className="px-5 py-2 text-green-600 border rounded-sm border-green-500 hover:bg-green-500 hover:text-white"
           >
             Incluir Quadra
           </Link>
-          
 </div>
 
       
